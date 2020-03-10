80c93d7cd2cdc33b91f5afe6855a3250523ed96e
Eric Engman <eengman19@gmail.com>
2020-03-10 00:16:22 -0700

    added in some icons

--- shared/header.js
+++ shared/header.js
@@ -1,23 +1,39 @@
 /* eslint-disable prettier/prettier */
 import React from 'react';
 import { StyleSheet, Text, View, Button, TouchableOpacity} from 'react-native';
-//import { Icon } from 'react-native-elements';
+import  Icon  from 'react-native-vector-icons/Ionicons'; 
 export default function Header({ navigation, title }) {
 
     const openMenu = () => {
         navigation.openDrawer();
     }
+    let screenIcon = "md-home";
+    if(title === "Completed"){
+        screenIcon = "md-checkmark-circle-outline";
+    }
 
     return(
         <View style={styles.header}>
             
-            <TouchableOpacity style={{color: '#5c5a5a', borderWidth: 1, borderColor: 'white'}} onPress={openMenu}>
-                <Text style={{fontSize: 20}}>||||</Text>
+            <TouchableOpacity style={{color: '#5c5a5a'}} onPress={openMenu}>
+            <Icon
+                name="md-menu"
+                size={35}
+                        />
             </TouchableOpacity> 
-            
+
+            <View style={{paddingLeft: 25}}>
+                <Icon
+                name= {screenIcon}
+                size={35}
+                        />
+            </View>
+
             <View>
                 <Text style={styles.headerText}>{ title }</Text>
             </View>
+
+
         </View>
     );
 }
@@ -36,7 +52,7 @@ const styles = StyleSheet.create({
         fontSize: 26,
         color: '#5c5a5a',
         fontFamily: 'monospace',
-        //letterSpacing: 3,
+        letterSpacing: 2,
         padding: 24
     },
 });
